52920749a2a3917e7583b05e979419dc4c92aa0d
scottrepreneur <me@stwoh.com>
2020-05-29 14:30:26 -0500

    create tokens with some generic defaults

--- src/components/PersonalToken/index.jsx
+++ src/components/PersonalToken/index.jsx
@@ -1,5 +1,34 @@
 import React, { useState } from "react"
+import { withRouter } from "react-router-dom"
 import styled from "styled-components"
+import { useFactoryContract, useWeb3React } from "../../hooks"
+import Web3Status from "../Web3Status"
+import { utils } from "ethers"
+
+import { Link } from "../../theme"
+
+const tokenFormItems = [
+  {
+    name:"tokenName",
+    type: "text",
+    placeholder:"Personal Token Name",
+  },
+  {
+    name: "symbol",
+    type: "text",
+    placeholder: "What are your Initials? (Token Symbol)",
+  },
+  {
+    name: "tos", 
+    type: "text",
+    placeholder: "Terms of Service Link (Optional)" 
+  },
+  {
+    name: "email",
+    type: "email",
+    placeholder: "Email (Optional)"
+  }
+]
 
 const Wrapper = styled.div`
   display: grid;
@@ -12,28 +41,105 @@ const Description = styled.div`
   grid-area: description;
   width: 80%;
   margin: auto;
+  line-height: 1.7rem;
+
+  & > p {
+    font-size: 0.938rem;
+
+    & > a {
+      color: ${({ theme }) => theme.uniswapPink};
+      text-decoration: none;
+
+      :hover {
+        text-decoration: underline;
+      }
+    }
+  }
 `
 
 const TokenForm = styled.div`
   grid-area: form;
   width: 80%;
   margin: auto;
+  
+  & > form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    & > input {
+      width: 80%;
+      line-height: 1.5rem;
+      margin-bottom: 3.5rem;
+      border-radius: .25rem;
+      border: 1px solid #000000;
+      color: #3F3F3F;
+      font-size: 0.875rem;
+      padding: 1rem 0.5rem;
+    }
+  }
 `
 
 const Button = styled.a`
   display: block;
-  max-width: 150px;
+  max-width: 80px;
   padding: 1rem 3rem;
   background-image: linear-gradient(40deg, #28BCFD 20%, #1D78FF 51%, #28BCFD 90%);
   border-radius: 5px;
+  text-align: center;
 
   :hover {
     cursor: pointer;
   }
 `
 
-export default function PersonalToken() {
-  const [tokenForm, setTokenForm] = useState({})
+function PersonalToken({ history }) {
+  const [tokenForm, setTokenForm] = useState({
+    "name": "",
+    "symbol": "",
+    "stamp": "test",
+    "decimals": 6,
+    "initialSupply": 240000,
+    "cap": 2400000
+  })
+
+  const { account } = useWeb3React()
+
+  const factory = useFactoryContract();
+
+  const handleChange = (event) => {
+    setTokenForm({
+      ...tokenForm,
+      [event.target.id]: event.target.value
+    });
+  }
+
+  const handleSubmit = (event) => {
+    // alert('A name was submitted: ' + newBadge.name);
+    event.preventDefault();
+    onCreateToken(tokenForm)
+  }
+
+  async function onCreateToken(token) {
+    console.log(token)
+    console.log(account)
+    let result = await factory.newLexToken(
+      token.name,                                     //  new token name
+      token.symbol,                                   //  new token symbol
+      token.stamp,                                    //  new token stamp?
+      token.decimals,                                 //  [constant] new token decimals 
+      token.cap,                                      //  [constant] new token maximum supply cap
+      token.initialSupply,                            //  [constant] new token initial supply
+      account,                                        //  new token owner
+      true,                                           //  [constant] new token governed by LexDAO
+      {
+        value: utils.parseEther("0.0009")             //  lexdao tribute
+      }
+    )
+    
+    console.log(result);
+    return result;
+  }
 
   return(
     <Wrapper>
@@ -43,40 +149,70 @@ export default function PersonalToken() {
       </div>
       <Description>
         <h3>Why use "LexDAO Certified" personal tokens?</h3>
-
-        For one, these are the tokens we use, and we're a bunch of lawyers.
+        <p>For one, these are the tokens we use, and we're a bunch of lawyers.</p>
 
         <h3>Standardized Values:</h3>
-        1 Token = 1 Minute of Your Time
-
-        Initial Offering: 240,000 Tokens (1,000 Hrs)
-
-        Decimal places: 6
+        <p>
+          1 Token = 1 Minute of Your Time<br />
+          Initial Offering: 240,000 Tokens (1,000 Hrs)<br />
+          Decimal places: 6
+        </p>
 
         <h3>What about disputes?</h3>
-
-        If you're having trouble with a personal token transaction? LexDAO has your back with dispute resolution built in.
-
-        Need to start a dispute? Click Here
+        <p>If you're having trouble with a personal token transaction? LexDAO has your back with dispute resolution built in.</p>
+        <p>Need to start a dispute?&nbsp;
+          <Link onClick={() => history.push('/personal-token-dispute')}>
+            Click Here
+          </Link>
+        </p>
 
         <h3>Some legal stuff:</h3>
-
-        Learn why you might want to attach your email.
-
-        If you do not provide your own your Terms of Service link, your token will be governed by LexDAO Default Personal Token TOS
-
-        By clicking "Mint" you agreed to the LexDAO Terms of Service.
+        <p>
+          <Link onClick={() => history.push('/why-email')}>
+            Learn why you might want to attach your email.
+          </Link>
+        </p>
+        <p>If you do not provide your own your Terms of Service link, your token will be governed by&nbsp;
+          <Link onClick={() => history.push('/personal-token-dispute')}>
+            LexDAO Default Personal Token TOS
+          </Link>
+        </p>
+        <p>By clicking "Mint" you agreed to the&nbsp;
+          <Link onClick={() => history.push('/personal-token-dispute')}>
+            LexDAO Terms of Service
+          </Link>.
+        </p>
       </Description>
       <TokenForm>
+        
         <div>
-          Step 1: Connect a Web3 Wallet
-          <Button>Connect A Wallet</Button>
-          Step 2: Complete the following form
+          <h3>Step 1: Connect a Web3 Wallet</h3>
+          <div style={{ maxWidth: '200px' }}>
+            <Web3Status />
+          </div>
+          <h3>Step 2: Complete the following form</h3>
         </div>
         <form>
-          <input id="tokenName" type="text" defaultValue="My Personal Token"/>
+          {tokenFormItems.map(item => {
+            return(
+              <input
+                key={item.name}
+                id={item.name}
+                type={item.type}
+                placeholder={item.placeholder}
+                onChange={handleChange}
+              />
+            )
+          })}
         </form>
+        <Button
+          onClick={handleSubmit}
+        >
+          Mint
+        </Button>
       </TokenForm>
     </Wrapper>
   )
 }
+
+export default withRouter(PersonalToken)
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,9 @@
 import { injected, walletconnect, walletlink, fortmatic, portis, torus } from '../connectors'
 
 export const FACTORY_ADDRESSES = {
-  1: '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95',
-  3: '0x9c83dCE8CA20E9aAF9D3efc003b2ea62aBC08351',
-  4: '0xf5D915570BC477f9B8D6C0E980aA81757A3AaC36',
-  42: '0xD3E51Ef092B2845f10401a0159B2B96e8B6c3D30'
+  1: '0xAD5B189E64f3D972dec81F286b6101782aD37943',
+  4: '0x4a29ae06056873c436bd53301620270328757b87',
+  42: '0xD3E51Ef092B2845f10401a0159B2B96e8B6c3D30' // needs updated for kovan if applicable
 }
 
 export const SUPPORTED_THEMES = {
